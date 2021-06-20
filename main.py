7f72d32d4397626b037ae55862522993411b8bc7
KLZRespectoR <alhmeidi@hotmail.com>
2021-06-20 14:13:26 +0200

    to present

--- main.py
+++ main.py
@@ -6,66 +6,81 @@ print("Length of the Talk: 11:02")
 print("Source: https://www.ted.com/talks/greta_thunberg_the_disarming_case_to_act_right_now_on_climate_change#t-259530")
 
 
+import nltk
+from nltk.corpus import wordnet
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import string
+import re
+
+
 #Reading the Text:
 text= open('text.txt', encoding='utf-8').read()
 print("the length of the speech before cleaning:", len(text))
+print('\n')
 
-
-#Cleaning the Text (the Talk):
+#Cleaning the Text (the Talk/ Speeches):
 
 #1- convert the Text to lower case:
-import string
+
 lower_case= text.lower()
 #print(lower_case)
 
-# Remove the timer:
-import re
+# 2- Remove the timer:
+
 def timer_eliminator(text):
     text = re.sub(r"[0-9][0-9]:[0-9][0-9]", '', text)
     return text
 #print("Removing time:\n")
-no_time_text = timer_eliminator(lower_case)
-#print(no_time_text)
+no_timer_text = timer_eliminator(lower_case)
+#print(no_timer_text)
+
+# 3- Remove words in Brackets:
 
 def brackets_eliminator(text):
     text = re.sub(r"[(]applause[)]", '', text)
     text = re.sub(r"[(]laughter[)]", '', text)
     return text
 
-print("Removing brackets:\n")
-#print(brackets_eliminator(no_time_text))
+#print("Removing brackets:\n")
+#print(brackets_eliminator(no_timer_text))
+no_brackets_text = brackets_eliminator(no_timer_text)
 
-#Remove (applause)/ (Laughter):
 
+# 4- Remove Punctuation:
 
-#2- Remove Punctuation:
-cleaned_text= lower_case.translate(str.maketrans('','',string.punctuation))
-print(cleaned_text)
-print("the length of the text without punctuation:", len(cleaned_text))
+cleaned_text= no_brackets_text.translate(str.maketrans('','',string.punctuation))
+#print("cleaned text:\n")
+#print(cleaned_text)
+#print("the length of the text without punctuation:", len(cleaned_text))
+#print('\n')
 
-#3- Remove spaces and lines breaks:
+# 5- Remove spaces and lines breaks:
 
-def preprocess_text(text):
+def space_eliminator(text):
     text= re.sub('\n+', ' ', text)
     text= re.sub(' +', ' ', text)
 
     return text
 
-print(preprocess_text(cleaned_text))
-print(len(preprocess_text(cleaned_text)))
+#print("Removing Speces:\n")
+#print(space_eliminator(cleaned_text))
+#print('\n')
+#print("the length of the text without spaces:", len(space_eliminator(cleaned_text)))
 
 
-# Tokenization:
-import nltk
-from nltk.tokenize import word_tokenize
+# 6- Tokenization:
+
 tokenized_text= word_tokenize(cleaned_text)
-print(tokenized_text)
+#print('\n')
+#print("Tokenized Text:", tokenized_text)
 
-#remove stop words:
-from nltk.corpus import wordnet
-from nltk.corpus import stopwords
+# 7- Remove stop words:
+
 stop_words= stopwords.words('english')
-print(stop_words)
+#print('\n')
+#print("Stop Words:\n")
+#print(stop_words)
 
 
 text_without_stopwords= []
@@ -73,38 +88,48 @@ for word in tokenized_text:
     if word not in stop_words:
         text_without_stopwords.append(word)
 
-print(text_without_stopwords)
+#print('\n')
+#print("Text without stop words:\n")
+#print(text_without_stopwords)
+#print("The length of the Text without stop words:", len(text_without_stopwords))
 
-print(len(text_without_stopwords))
+# THE READY TO GO TEXT:
+#print("The Final version of the text:", text_without_stopwords)
+#print("The Final version of the text:", len(text_without_stopwords))
+final_text= text_without_stopwords
+
+# Build the Sentiment analyser:
+
+# 1- Reading the texts:
 
 positve_list = word_tokenize(open('positive-words.txt', encoding='utf-8').read())
 
 negative_list = word_tokenize(open('negative-words.txt', encoding='utf-8').read())
 
+# 2- Building the function:
 
-def sentiment_analyzer(text):
+def sentiment_analyser(final_text):
     positive = 0
     negative = 0
 
-    for word in text:
+    for word in final_text:
         if word in positve_list:
             positive = positive +1
         if word in negative_list:
             negative = negative +1
 
     if (positive > negative):
-        print("Text is positiv")
-    elif(negative > positive):
-        print("Text is negative")
+        print()
+        #print("Text is positiv")
+    elif (negative > positive):
+        print()
+        #print("Text is negative")
     else:
-        print("Text is neutral")
+        print()
+        #print("Text is neutral")
 
+    #print("positive words number: ", positive)
+    #print("negative words number: ", negative)
 
-    p_ratio = (int)(1000* positive/len(text))
-    n_ratio = (int)(1000* negative/len(text))
-    print("positive words ratio: ", p_ratio/1000)
-    print("negative words ratio: ", n_ratio/1000)
-
-
-sentiment_analyzer(text_without_stopwords)
+sentiment_analyser(final_text)
 
